cefccd2af5e5c15d26920132d7b4e9f7cbf40498
ShobhitRatan <shobhit.ratan@baruchmail.cuny.edu>
2020-10-30 16:06:08 -0400

    primitive types problems completed

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,8 +1,19 @@
 from test_framework import generic_test
-
+import math 
 
 def is_palindrome_number(x: int) -> bool:
-    # TODO - you fill in here.
+
+    if x <= 0:
+        return x == 0
+
+    num_digits = math.floor(math.log10(x)) + 1
+    msd_mask = 10**(num_digits - 1)
+    for i in range(num_digits // 2):
+        if x // msd_mask != x % 10:
+            return False
+        x %= msd_mask  # Remove the most significant digit of x.
+        x //= 10  # Remove the least significant digit of x.
+        msd_mask //= 100
     return True
 
 
--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,10 +1,30 @@
 from test_framework import generic_test
 
+# Brute force approach 
+# def parity(x: int) -> int:
+#     result = 0 
+#     while x:
+#         result ^= x & 1 
+#         x >>= 1
+#     return result 
 
+# 1st Alternative approach 
+# def parity(x: int) -> int:
+#     result = 0
+#     while x: 
+#         result ^= 1 
+#         x &= x - 1 #Drops the lowest set bit of x. 
+#     return result 
+
+# 2nd Alternative approach using XOR and Associativity 
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
-
+    x ^= x >> 32 
+    x ^= x >> 16
+    x ^= x >> 8 
+    x ^= x >> 4 
+    x ^= x >> 2 
+    x ^= x >> 1 
+    return x & 0x1 
 
 if __name__ == '__main__':
     exit(generic_test.generic_test_main('parity.py', 'parity.tsv', parity))
